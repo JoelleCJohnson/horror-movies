bc3b347b422042f6f2b79ca17d52643d1cdc8e8f
Joelle  Johnson <joellej221@gmail.com>
2023-10-27 14:59:09 -0400

    horror movies site using sampleAPIs

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Halloween Horror Movies</h1>
+            <p>Watch them-if you dare </p>
+        </header>
+    )
+}
